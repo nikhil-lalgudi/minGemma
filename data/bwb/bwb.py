10b58de9cafdd8128db301dbc37498cd39440887
Nikhil Lalgudi Vaidyanathan <144494028+nikhil-lalgudi@users.noreply.github.com>
2024-06-25 14:39:39 +0530

    non specific datasets

---
+++ data/bwb/bwb.py
@@ -0,0 +1,6 @@
+# reference: https://huggingface.co/datasets/billion-word-benchmark/lm1b
+
+import os
+import tqdr
+
+num_proc = 8 # number of processes to use
--- data/wikitext/wikitext.py
+++ data/wikitext/wikitext.py
@@ -1,6 +1,9 @@
+## reference: https://huggingface.co/datasets/Salesforce/wikitext-103
+
 import os
 import numpy as np
 import tqdm # progress bar -- helps with debugging
 
 # TODO: decide what other things we can use for the dataset
 
+num_proc = 8 # number of processes to use
